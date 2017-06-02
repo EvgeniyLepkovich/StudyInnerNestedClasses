68ad6a25c96ac59cfa8fa05c0fcadd022fe38412
Yayheniy_Lepkovich <Yayheniy_Lepkovich@epam.com>
2017-06-02 13:34:15 +0300

    Local class in enum example, move main to local package and renaim to 'AppConfig'

---
+++ src/java/by/epam/local/AppConfig.java
@@ -0,0 +1,33 @@
+package by.epam.local;
+
+import java.util.List;
+
+/**
+ * Created by Yayheniy_Lepkovich on 6/2/2017.
+ */
+public class AppConfig {
+    public static void main(String[] args){
+        String[] numbers = { "5298758967", "5298417496", "296798741", "aaa", "257418588" };
+
+//        Java 8
+//        PhoneNumberValidator.getValidNumbers(numbers).stream().forEach(i -> System.out.println("Number " + i + " is valid.") );
+        List<String> validNumbers = PhoneNumberValidator.getValidNumbers(numbers);
+        for (String number : validNumbers) {
+            System.out.println("Number " + number + " is valid.");
+        }
+
+        RegionPhoneNumberValidator CisPhoneNumberValidator = RegionPhoneNumberValidator.CIS;
+        validNumbers = CisPhoneNumberValidator.ValidatePhones(numbers);
+        System.out.println("Cis valid numbers:");
+        for (String number : validNumbers) {
+            System.out.println("Number " + number + " is valid.");
+        }
+
+        RegionPhoneNumberValidator EuPhoneNumberValidator = RegionPhoneNumberValidator.EU;
+        validNumbers = EuPhoneNumberValidator.ValidatePhones(numbers);
+        System.out.println("Eu valid numbers:");
+        for (String number : validNumbers) {
+            System.out.println("Number " + number + " is valid.");
+        }
+    }
+}
